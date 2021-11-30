a879d4cab13a1fa49a84aff6534500ab24b0704d
Raviraj <60103458+Ravirajayya@users.noreply.github.com>
2021-11-30 15:44:17 +0530

    Add files via upload
    Update

--- lombok/src/main/java/com/projectLombok/lombok/LombokApplication.java
+++ lombok/src/main/java/com/projectLombok/lombok/LombokApplication.java
@@ -3,11 +3,27 @@ package com.projectLombok.lombok;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 @SpringBootApplication
 public class LombokApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LombokApplication.class, args);
+		
+		lombokTest();
 	}
 
+	public static void lombokTest() {
+		
+		System.out.println("======> Lombok: @Getter, @Setter <======");
+		TestGetterSetter lombok_1 = new TestGetterSetter();
+		lombok_1.execute();
+		
+		System.out.println("======> Lombok: @NoArgsConstructor, @AllArgsConstructor, @ToString <======");
+		UserModel lombok_2 = new UserModel();
+		lombok_2.execute();
+	}
 }
