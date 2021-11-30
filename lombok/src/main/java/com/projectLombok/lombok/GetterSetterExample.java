4e93a795a1c408ee606a13a9c514d0b13dd8d58b
Raviraj <60103458+Ravirajayya@users.noreply.github.com>
2021-11-30 15:18:15 +0530

    Add files via upload
    Lombok annotations

---
+++ lombok/src/main/java/com/projectLombok/lombok/GetterSetterExample.java
@@ -0,0 +1,24 @@
+package com.projectLombok.lombok;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @Getter @Setter 
+public class GetterSetterExample {
+
+	private String username;
+	private String password;
+}
+
+class TestGetterSetter {
+	
+	public void demo() {
+		GetterSetterExample lombokEx = new GetterSetterExample();
+		lombokEx.setUsername("Raviraj");
+		lombokEx.setPassword("P@assword");
+		
+		System.out.println("User name: " + lombokEx.getUsername());
+		System.out.println("Password: " + lombokEx.getPassword());
+	}
+}
